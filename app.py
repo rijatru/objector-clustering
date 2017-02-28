c651bcec0554b831da4d47f81a123f362e7ca770
rijatru <rijatru@gmail.com>
2017-02-27 21:28:28 -0500

    Test 2

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-
-@app.route('/api/v1.0/cluster', methods=['GET'])
-def index():
-    return 'OK!'
-
-app.run()
+print("This line will be printed.")
