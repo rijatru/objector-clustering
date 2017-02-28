eda080b5e9502298e7ebf17f5ff8c8c943ee69c9
rijatru <rijatru@gmail.com>
2017-02-27 21:40:16 -0500

    Test 2

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def source():
-    html = 'Hello World!'
-    return html
+    return jsonify({'tasks': tasks})
