fecd2741f61efcae3866593b0dc0803b1fee9635
rijatru <rijatru@gmail.com>
2017-02-27 20:25:45 -0500

    Test 2

--- app.py
+++ app.py
@@ -2,24 +2,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
-
 
 @app.route('/api/v1.0/cluster', methods=['GET'])
 def index():
-    return jsonify({'tasks': tasks})
+    return 'OK!'
 
 app.run()
