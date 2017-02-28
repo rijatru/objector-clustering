e44764eca3a4b0ecae7649cd9c4b007667e70a7e
rijatru <rijatru@gmail.com>
2017-02-27 19:00:46 -0500

    Test

--- service/main.py
+++ service/main.py
@@ -1,1 +1,17 @@
-print("This line will be printed.")
+from flask import Flask, render_template
+import requests
+import json
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    r = requests.get('http://api.icndb.com/jokes/random')
+    data = json.loads(r.text)
+    joke = data['value']['joke']
+    return render_template('index.html', joke=joke)
+
+
+app.run(debug=True)
