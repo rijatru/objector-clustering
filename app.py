f0b7bd8275eea7f41b2bdb78860643ce85d3d581
rijatru <rijatru@gmail.com>
2017-02-27 19:35:04 -0500

    Test 2

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
 
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return jsonify({'tasks': tasks})
 
 app.run()
